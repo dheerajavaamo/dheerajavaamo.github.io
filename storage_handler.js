1c7c0b2674203fbbdf3af71fa12cead85d13d7c4
Dheeraj Samala <dheeraj@avaamo.com>
2020-05-11 10:51:35 +0530

    new fields

--- storage_handler.js
+++ storage_handler.js
@@ -14,56 +14,51 @@ function getStorage(url) {
                 if (obj.key == "person") {
                     var first_name = obj.value;
                 }
-                if (obj.key == "last_name_1") {
-                    var last_name = obj.value;
-                }
-                if (obj.key == "insurance_type_1") {
-                    var insurance_type = obj.value;
-                }
-                if (obj.key == "email") {
-                    var email = obj.value;
-                }
-                if (obj.key == "account_number_1") {
+                else if (obj.key == "account_number_1") {
                     var account_number = obj.value;
                 }
-                if (obj.key == "drivers_license") {
-                    var drivers_license = obj.value;
-                }
-                if (obj.key == "custom_city_1") {
-                    var city = obj.value;
-                }
-                if (obj.key == "custom_state_1") {
-                    var state = obj.value;
-                }
-                if (obj.key == "location_1") {
-                    var location = obj.value;
-                }
-                if (obj.key == "towed") {
-                    var towed = obj.value;
-                }
-                if (obj.key == "other_vehicle_information") {
-                    var other_vehicle_information = obj.value;
-                }
-                
-                if (obj.key == "zip_code") {
-                    var zip_code = obj.value;
-                }
-                if (obj.key == "custom_country_1") {
-                    var country = obj.value;
-                }
-                if (obj.key == "phone") {
+                else if (obj.key == "phone") {
                     var phone = obj.value;
                 }
-                if (obj.key == "datetime") {
-                    var datetime = obj.value;
+                else if (obj.key == "accident_location") {
+                    var accident_location = obj.value;
                 }
-                if (obj.key == "car_make_1") {
+                else if (obj.key == "accident_city") {
+                    var accident_city = obj.value;
+                }
+                
+                else if (obj.key == "car_make_1") {
                     var car_make = obj.value;
                 }
-                if (obj.key == "car_model_1") {
+                else if (obj.key == "car_model_1") {
                     var car_model = obj.value;
                 }
-                if (obj.key == "accident_details_1" && obj.value) {
+
+                else if (obj.key == "other_passengers_in_car") {
+                    var other_passengers_in_car = obj.value;
+                }
+                else if (obj.key == "other_vehicle_information") {
+                    var other_vehicle_information = obj.value;
+                }
+                else if (obj.key == "injuries") {
+                    var injuries = obj.value;
+                }
+                else if (obj.key == "towed") {
+                    var towed = obj.value;
+                }
+                else if (obj.key == "datetime") {
+                    var datetime = obj.value;
+                }
+                else if (obj.key == "time_of_accident") {
+                    var time_of_accident = obj.value;
+                }
+                else if (obj.key == "damage_location") {
+                    var damage_location = obj.value;
+                }
+                else if (obj.key == "how_many_vehicles_damaged") {
+                    var how_many_vehicles_damaged = obj.value;
+                }
+                else if (obj.key == "accident_details_1" && obj.value) {
                     var accident_details = JSON.parse(obj.value).join(". ");
                     console.log(accident_details);
                 }
@@ -75,18 +70,22 @@ function getStorage(url) {
             // console.log(html);
             $("#first-name").attr("value", first_name);
             $("#account-number").attr("value", account_number);
-            $("#drivers-license").attr("value", drivers_license);
-            // $("#city").attr("value", city);
-            // $("#state").attr("value", state);
             $("#location").attr("value", location);
             $("#towed").attr("value", towed);
             $("#other_vehicle_information").attr("value", other_vehicle_information);
-            // $("#zip-code").attr("value", zip_code);
             $("#phone").attr("value", phone);
             $("#datetime").attr("value", datetime);
             $("#car-make").attr("value", car_make);
             $("#car-model").attr("value", car_model);
             $("#accident-details").html(accident_details);
+
+            $("#accident_location").attr("value", accident_location);
+            $("#accident_city").attr("value", accident_city);
+            $("#other_passengers_in_car").attr("value", other_passengers_in_car);
+            $("#injuries").attr("value", injuries);
+            $("#time_of_accident").attr("value", time_of_accident);
+            $("#damage_location").attr("value", damage_location);
+            $("#how_many_vehicles_damaged").attr("value", how_many_vehicles_damaged);
 
             let firstBlankField = $('select,input,textarea').filter(function() { return $(this).val() == ""; })[0];
             if(firstBlankField){
--- voice_client.js
+++ voice_client.js
@@ -150,7 +150,7 @@ function stopRecording() {
 
     microphone.classList.remove("active");
 
-    if(processedUtterance && !isTraining) sendMessage(processedUtterance);
+    if(processedUtterance && !isTraining) sendMessage(postProcessUtterance(processedUtterance));
     processedUtterance = "";
     resetSpeechData();
 }
@@ -204,14 +204,30 @@ socket.on('speechData', function (data) {
 });
 
 function postProcessUtterance(text){
-    replacements.forEach(r => {
-        Object.keys(r).forEach(key => {
-            text = text.replace(r[key], key);
-        });
+    let extractedDates = chrono.parse(text);
+
+    let extracted_time = "";
+
+    extractedDates.forEach(d => {
+        if(extracted_time || d.text.length === 2){
+            return;
+        }
+        if(d.start && d.start.knownValues && d.start.knownValues.hour){
+            let am_pm = "am"
+            if(d.start.knownValues.hour > 12){
+                d.start.knownValues.hour -= 12;
+                am_pm = "pm"
+            }
+            if(!d.start.knownValues.minute < 10){
+                d.start.knownValues.minute = d.start.knownValues.minute + "0";
+            }
+            extracted_time = `${d.start.knownValues.hour}:${d.start.knownValues.minute} ${am_pm}`;
+        }
     });
-
+    if(extracted_time){
+        text = text + "time_of_incident:" + extracted_time;
+    }
     return text;
-
 }
 
 
